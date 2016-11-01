17d33239ff5137621718c3bc86c380c7355eea36
Carlos <Carlos@Marge.casa>
2016-11-01 22:42:11 +0100

    Init Project

---
+++ ImagenioXmlTv/src/main/java/es/espinaco/ImagenioXmlTvApplication.java
@@ -0,0 +1,12 @@
+package es.espinaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagenioXmlTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagenioXmlTvApplication.class, args);
+	}
+}
